81d13af58e88764fc5f728aefea52dd97ae04098
shivam pandey <51897113+shivam-pandey99@users.noreply.github.com>
2021-02-01 21:57:50 +0530

    project setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+return (
+<div className="App">
+  <div>
+  <div className="websiteName">
+  <h1>Shivam Pandey</h1>
+  <div className="list">
+    <ul>
+      <li>home</li>
+      <li>about</li>
+      <li>projects</li>
+      <li>skills</li>
+      <li>social-Media</li>
+      <li>contact</li>
+    </ul>
+   </div>
+  </div>
+ </div>
+</div>
+);
 }
 
 export default App;
+ 
