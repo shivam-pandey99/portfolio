245447a149eb7c86a68e5f42db988acc20f57d08
shivam pandey <51897113+shivam-pandey99@users.noreply.github.com>
2021-02-01 23:27:32 +0530

    navbar completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
+import Navbar from './component/Navbar'
 
 function App() {
 return (
-<div className="App">
-  <div>
-  <div className="websiteName">
-  <h1>Shivam Pandey</h1>
-  <div className="list">
-    <ul>
-      <li>home</li>
-      <li>about</li>
-      <li>projects</li>
-      <li>skills</li>
-      <li>social-Media</li>
-      <li>contact</li>
-    </ul>
-   </div>
-  </div>
- </div>
-</div>
+<Navbar/>
 );
 }
 
